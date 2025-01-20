d5b23e73f9f8f0750c82dbb00aa2c2f0be3fc7f6
Ravali Manthini <manthenravali97@gmail.com>
2025-01-21 02:09:06 +0530

    checking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.role = localStorage.getItem('role');
-    //this.router.navigate(['/home']);
+    this.router.navigate(['/home']);
     if (!this.role) {
       this.router.navigate(['/home']);
     }
